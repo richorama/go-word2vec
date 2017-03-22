e551c08230b548687f29eff8fff6a7262c5ec707
Richard Astbury <richard.astbury@gmail.com>
2017-03-22 10:04:01 +0000

    add, subtract, distance and nearest

--- model.go
+++ model.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"math"
 )
 
 type WordVector struct {
@@ -16,23 +17,16 @@ type WordVector struct {
 
 type WordVectors []WordVector
 
-func (vectors WordVectors) findVector(word string) (bool, WordVector){
-	for _,vector := range vectors{
-		if vector.word == word {
-			return true, vector
-		}
-	}
-	return false, WordVector{word, []float32{}}
-}
-
 func main() {
 	model := load("model.txt")
 
-	found, vector := model.findVector("whale")
+	found, vector := model.FindVector("whale")
 
 	if found {
 		fmt.Println("found", vector.word)
+		vector.Add(vector)
 	}
+
 
 	fmt.Println(len(model))
 }
@@ -75,4 +69,60 @@ func parseLine(line []string) WordVector {
 		vector = append(vector, float32(val))
 	}
 	return WordVector{line[0], vector}
+}
+
+
+func (vectors WordVectors) FindVector(word string) (bool, WordVector){
+	for _,vector := range vectors{
+		if vector.word == word {
+			return true, vector
+		}
+	}
+	return false, WordVector{word, []float32{}}
+}
+
+func (vector2 WordVector) Add(vector1 WordVector) WordVector {
+	returnArray := []float32{}
+	for i := 0; i < len(vector1.vectors); i++{
+		returnArray = append(returnArray, vector1.vectors[i] + vector2.vectors[i])
+	}
+	return WordVector{ vector1.word + " + " + vector2.word, returnArray}
+}
+
+func (vector2 WordVector) Subtract(vector1 WordVector) WordVector {
+	returnArray := []float32{}
+	for i := 0; i < len(vector1.vectors); i++{
+		returnArray = append(returnArray, vector1.vectors[i] - vector2.vectors[i])
+	}
+	return WordVector{ vector1.word + " - " + vector2.word, returnArray}
+}
+
+func (vector2 WordVector) Distance(vector1 WordVector) float32 {
+	distance := float64(0)
+	for i := 0; i < len(vector1.vectors); i++{
+		distance += math.Pow(float64(vector1.vectors[i] - vector2.vectors[i]), float64(2))
+	}
+	return float32(math.Sqrt(distance))
+}
+
+func (vectors WordVectors) Nearest(vector WordVector) (bool, WordVector){
+	if (len(vectors) == 0){
+		return false, vector
+	}
+
+	shortestDistance := math.MaxFloat64
+	shortestVector := vector
+
+	for _,vector2 := range vectors{
+		if vector2.word == vector2.word {
+			continue
+		}
+
+		thisDistance := float64(vector.Distance(vector2))
+		if (thisDistance < shortestDistance){
+			shortestDistance = thisDistance
+			shortestVector = vector2
+		}
+	}
+	return true, shortestVector
 }
